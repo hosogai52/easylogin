36d831920c35ff6734ee2e286c142258d3029942
hosogai52 <misaki7352@gmail.com>
2023-03-29 16:31:21 +0900

    Spring Bootで作成した簡易ログインサイト

---
+++ easylogin/src/test/java/com/example/example/easylogin/EasyloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.example.easylogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
